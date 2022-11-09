f5532e80acfc5a43424c1c054fc9ace27a9ddd23
LeoCavachini <cavachini240@gmail.com>
2022-11-09 16:17:55 -0300

    chore: init project

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const pokemonRouter = require('./routes/pokemon');
+const swaggerDocs = require('./swagger.json');
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use('/image', express.static(`${__dirname}/image`));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+app.use(pokemonRouter);
+
+mongoose
+  .connect('mongodb+srv://leosasuke:pikachu36@pokemongo.cmtjgsy.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+  })
+  //.connect('mongodb://db:27017/pokemon-api', {
+  //  useNewUrlParser: true
+  //})
+  .then(() => {
+    app.listen(3000, () => console.log('server is running'))
+  })
+  .catch(error => {
+    console.log(error);
+  });
