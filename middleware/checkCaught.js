59a0e27c72c02cddc8eda4e518ce8ed3538d139b
LeoCavachini <cavachini240@gmail.com>
2022-11-09 21:18:50 -0300

    fix: fixing

--- middleware/checkCaught.js
+++ middleware/checkCaught.js
@@ -2,21 +2,16 @@ const Caught = require('../components/caughtPokemon/model');
 
 
 const checkCaught = async (req, res, next) => {
-  const { name, imageUrl } = req.body;
-
-  const caught = new Caught({
-    name, 
-    imageUrl
-  });
+  const { name } = req.body;
 
   try {
     const pokemon = await Caught.findOne({ name: name})
     if (pokemon) {
-      return res.status(401).json({ message: "Você já capturou esse pokemon"})
+      return res.status(200).json({ message: "Você já capturou esse pokemon"})
     }
     next()
   } catch (error) {
-    res.status(400).json({ mesage: error})
+    res.status(500).json({ mesage: error})
   }
 
 }
